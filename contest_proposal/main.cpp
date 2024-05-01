8f16809b04f90c58fc66cdb6b6434e1e20399556
TuteMthCD <matias.civadda2342001@gmail.com>
2024-05-01 04:26:54 -0300

    ADD solve contest_proposal

---
+++ contest_proposal/main.cpp
@@ -0,0 +1,56 @@
+
+/*
+ * La idea de este codigo es resolver este problema
+ * https://codeforces.com/problemset/problem/1972/A
+ */
+
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+int getIntInput() {
+    std::string input;
+    input.reserve(10);
+
+    std::scanf("%s", input.data());
+
+    return atoi(input.data());
+}
+
+int main(int argc, char* argv[]) {
+    std::vector<int> An, Bn;
+    int AmountProblems, CountProblems;
+
+    std::scanf("%d", &AmountProblems);
+
+    for(int i = 0; i < AmountProblems; i++)
+        An.push_back(getIntInput());
+
+    for(int i = 0; i < AmountProblems; i++)
+        Bn.push_back(getIntInput());
+
+    std::sort(An.begin(), An.end());
+    std::sort(Bn.begin(), Bn.end());
+
+    auto IteA = An.begin();
+    auto IteB = Bn.begin();
+
+    while(IteA != An.end() - 1 && IteB != Bn.end() - 1) {
+        if(*IteA <= *IteB) {
+            IteA++;
+            IteB++;
+        } else {
+            An.erase(An.end() - 1);
+            An.insert(IteA, *IteB);
+            CountProblems++;
+            IteA = An.begin();
+            IteB = Bn.begin();
+        }
+    }
+    printf("%d", CountProblems);
+
+
+    return 0;
+}
