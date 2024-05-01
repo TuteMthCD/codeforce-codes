e72d1c0a0a9a1c5f3d0422cfb56c4e85e4d27bdc
TuteMthCD <matias.civadda2342001@gmail.com>
2024-05-01 01:38:12 -0300

    ADD resolve flipcoin game

--- coinflip/main.cpp
+++ coinflip/main.cpp
@@ -1,9 +1,54 @@
-#include <cstdio>
+/*
+ * La idea de este codigo es resolver este problema
+ * https://codeforces.com/problemset/problem/1972/B //nose si este link seguira funcionando
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
 int main(int argc, char* argv[]) {
+    int TestCases, AmountCoins;
+    std::vector<bool> Coins;
 
+    std::cin >> TestCases;
 
-    std::printf("hello word\n");
+    for(int i = 0; i < TestCases; i++) {
+        std::cin >> AmountCoins;
+
+        for(int j = 0; j < AmountCoins; j++) {
+            char coin = getchar();
+
+            if(coin == 'U' || coin == 'D')
+                Coins.push_back(coin == 'U');
+            else
+                j--;
+        }
+
+        bool WinAlice = false;
+        while(true) {
+            auto ite = std::find(Coins.begin(), Coins.end(), true);
+
+            WinAlice = !WinAlice;
+
+            if(ite != Coins.end()) {
+                Coins.erase(ite);
+
+                if(Coins.size() > 1) {
+                    for(int i = 1; i <= 2; i++)
+                        *(ite + i) = ((std::rand() % 1) == 1);
+                } else
+                    break;
+
+            } else
+                break;
+        }
+
+        Coins.clear();
+
+        std::cout << (WinAlice ? "YES" : "NO") << std::endl;
+    }
+
 
     return 0;
 }
